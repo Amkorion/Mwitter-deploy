1ab26fea27993db603cb70b3534f4e4792ce7b7e
Oleksandr Moroz <morozlex@gmail.com>
2023-04-06 20:02:55 +0300

    avatar corrections

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -9,13 +9,15 @@ const FollowBar = () => {
     return null;
   }
   return (
-    <div className="px-6 py-4 hidden lg:block">
+    <div className="px-6 py-4 hidden lg:block min-w-[320px]">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-xl text-white font-semibold">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
           {users.map((user: Record<string, any>) => (
             <div key={user.id} className="flex flex-row gap-4">
-              <Avatar userId={user.id} />
+              <div>
+                <Avatar userId={user.id} />
+              </div>
               <div className="flex flex-col">
                 <p className="text-white text-sm font-semibold">{user.name}</p>
                 <p className="text-neutral-400 text-sm">@{user.username}</p>

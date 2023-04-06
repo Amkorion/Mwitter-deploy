c027c68caa43ca95f4064a33f45354e8252e5745
Oleksandr Moroz <morozlex@gmail.com>
2023-04-06 08:39:16 +0300

    route changes

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -16,7 +16,7 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
     (event: any) => {
       event.stopPropagation();
 
-      const url = `users/${userId}`;
+      const url = `/users/${userId}`;
       router.push(url);
     },
     [router, userId]
